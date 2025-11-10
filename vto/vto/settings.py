8ffa03d6ecc57ba3af7e92b3f42d0b97549b1212
Alvinchisom <145863920+Alvinchisom@users.noreply.github.com>
2025-11-10 16:33:42 +0100

    Navber

--- vto/vto/settings.py
+++ vto/vto/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-^yc(vou)t3@9d6#pqmblf671e3$jq*-k6@9sl_dppp^@-*dc!g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost','127.0.0.1']
 CSRF_TRUSTED_ORIGINS = []
 
 
